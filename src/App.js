eef5fa73963e86c166f79ebbeaffa5ba1acd67d1
salaamander <saghevli@gmail.com>
2020-09-21 20:27:39 -0400

    initial commit, site placeholder

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import cowboys from './cowboys.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={cowboys} className="App-logo" alt="logo" />
+      </header>
+      Saam Aghevli 2020
+    </div>
+  );
+}
+
+export default App;
