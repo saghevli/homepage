bd1d45c333951259591d40dd9fcb94b1548dfd5e
salaamander <saghevli@gmail.com>
2020-09-22 00:24:00 -0400

    add production build and static.json

---
+++ build/precache-manifest.8d2abca103446158d8e5f57d68859bb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d97b28525d77d1b8a03e7515507ca9a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db8a27e64792d7dd6ea7",
+    "url": "/static/css/main.930bae4b.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db8a27e64792d7dd6ea7",
+    "url": "/static/js/main.c9edba37.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "c4abff078a86ec168768fee9dff61779",
+    "url": "/static/media/cowboys.c4abff07.jpg"
+  }
+]);
